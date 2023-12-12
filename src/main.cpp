5fc5aaf577d34cca92b924421798794c94302765
David Castrillon <khaldavid@Davids-MacBook-Air.local>
2023-12-12 16:27:27 +0100

    all

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,79 @@
+#include <U8g2lib.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <ArduinoJson.h>
+#include <HTTPClient.h>
+#include <WiFi.h>
+
+const char *ssid = "xsantcastx";
+const char *password = "1037647666";
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-#define SDA_PIN 21
-#define SCL_PIN 22
 #define SCREEN_ADDRESS 0x3C
+#define time_delay 15000
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
+
+// Function declaration
+void getBitcoinPrice();
+void connectToWiFi();
 
 void setup() {
-  Serial.begin(115200);  // Initialize serial communication
-  Wire.begin(SDA_PIN, SCL_PIN); // Initialize I2C communication
-  
-  if (!display.begin(SCREEN_ADDRESS)) {
-    Serial.println(F("Error inicializando la pantalla OLED"));
-    for (;;);
-  }
+  Wire.begin();
+  u8g2.begin();
 
-  Serial.println(F("OLED display initialized successfully"));
-  display.display();
-  delay(2000);
-  display.clearDisplay();
+  // Connect to WiFi
+  connectToWiFi();
+
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_ncenB08_tr);
+  u8g2.drawStr(0, 16, "Hello, Santiago!");
+  u8g2.sendBuffer();
+  delay(time_delay);
 }
 
 void loop() {
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 0);
-  display.println("¡Hola, ESP32!");
-  display.display();
-  delay(2000);
-  display.clearDisplay();
+  getBitcoinPrice();
+  delay(time_delay);  // Wait 15 seconds before updating the price
+}
+
+void connectToWiFi() {
+  Serial.begin(115200);
+
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nConnected to WiFi");
+}
+
+void getBitcoinPrice() {
+  HTTPClient http;
+  http.begin("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd");
+
+  int httpCode = http.GET();
+
+  if (httpCode == HTTP_CODE_OK) {
+    String payload = http.getString();
+    DynamicJsonDocument doc(1024);
+    deserializeJson(doc, payload);
+
+    // Extract Bitcoin price from JSON
+    float bitcoinPrice = doc["bitcoin"]["usd"];
+
+    // Display Bitcoin price on OLED
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_ncenB08_tr);
+    u8g2.setCursor(0, 16);
+    u8g2.print("Bitcoin Price: $");
+    u8g2.print(bitcoinPrice, 2);  // Display two decimal places
+    u8g2.sendBuffer();
+  }
+
+  http.end();
 }
